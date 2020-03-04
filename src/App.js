828143b712f10f203d0056434b854b2dded6d310
rguichette <ralphgui@gmail.com>
2020-03-03 23:41:29 -0500

    fixed palette routing

--- src/App.js
+++ src/App.js
@@ -4,24 +4,31 @@ import seedColors from "./seedcolors"
 import {generatePalette} from './colorhelpers'
 
 import {Route, Switch} from 'react-router-dom'
+import { Component } from 'react';
 
-function App() {
-  console.log(generatePalette(seedColors[4]));
-  
+class App extends Component {
+
+  findPalette(id){
+return seedColors.find(function(palette){
+  return palette.id === id;
+})
+  }
+  // console.log(generatePalette(seedColors[4]));
+  render(){
   return (
     <Switch>
     <Route exact path="/" render={()=>{
      return  <h1>Hello</h1>
     }}/>
-      <Route exact path="/palette/:id" render={()=>{
-     return  <h1>indi palette</h1>
-    }}/>
+      <Route exact path="/palette/:id" render={(routeProps)=>{
+     return  <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>}
+      }/>
+
       </Switch>
-//     <div className="App" >
-// <Palette palette={generatePalette(seedColors[4])}/>
-//     </div>
-    
-  );
-}
+
+  )
+    }
+  }
+  
 
 export default App;
